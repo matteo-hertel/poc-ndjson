7d71b12baba91642bf61a7927e57647e6296de59
Matteo Hertel <info@matteohertel.com>
2017-08-05 16:42:18 +0200

    fix server port

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(3321, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3321!');
 });
