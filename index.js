95d87d7550c85d88df90bd2b9b383583fc9e460d
Matteo Hertel <info@matteohertel.com>
2017-08-05 16:47:38 +0200

    add ndjson streaming endpoint:

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/api', (req, res) => {
+  let readStream = fs.createReadStream(__dirname + '/todos.ndjson');
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
+  //setup headers
+  res.writeHead(200, { 'Content-Type': 'application/ndjson' });
+
+  readStream.on('open', () => {
+    readStream.pipe(res); //pipe stream to response object
+  });
 });
 
 app.listen(3321, () => {
