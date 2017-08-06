a469a6d0015a0413e040cff246a2265d43b2560c
Matteo Hertel <info@matteohertel.com>
2017-08-06 16:48:38 +0200

    using ndjson library

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const ndjson = require("ndjson");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api', (req, res) => {
-  let readStream = fs.createReadStream(__dirname + '/todos.ndjson');
+app.get('/', (req, res) => {
+  let readStream = fs.createReadStream(__dirname + '/todos.ndjson').pipe(ndjson.parse());
 
-  //setup headers
-  res.writeHead(200, { 'Content-Type': 'application/ndjson' });
+  const chunks = [];
+  readStream.on('data', (data) => {
+    chunks.push(JSON.stringify(data));
+  });
 
-  readStream.on('open', () => {
-    readStream.pipe(res); //pipe stream to response object
+  readStream.on('end', () => {
+    var id = setInterval(() => {
+      if (chunks.length) {
+        res.write(chunks.shift() + '\n');
+      } else {
+        clearInterval(id);
+        res.end();
+      }
+    }, 500);
   });
 });
 
